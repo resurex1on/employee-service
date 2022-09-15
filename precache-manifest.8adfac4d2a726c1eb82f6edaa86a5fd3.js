4f63be2912e360d1c224d7dddf88e6929e53d12b
Uladzislau Liavonchyk <uladzislau.liavonchyk@sap.com>
2022-09-15 18:06:39 +0300

    Updates

---
+++ precache-manifest.8adfac4d2a726c1eb82f6edaa86a5fd3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8416a34226ecda48ce45",
+    "url": "/employee-service/static/css/main.81cbbbb1.chunk.css"
+  },
+  {
+    "revision": "8416a34226ecda48ce45",
+    "url": "/employee-service/static/js/main.8416a342.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/employee-service/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "9961a3e636b62809685d",
+    "url": "/employee-service/static/js/2.9961a3e6.chunk.js"
+  },
+  {
+    "revision": "257c61f81dc7e7b2a667",
+    "url": "/employee-service/static/js/runtime~main.257c61f8.js"
+  },
+  {
+    "revision": "567918c659af198e2aba1e2774352a7d",
+    "url": "/employee-service/static/media/user.567918c6.jpg"
+  },
+  {
+    "revision": "bbdbe0261c61018ca034508388c459d7",
+    "url": "/employee-service/static/media/user2.bbdbe026.jpg"
+  },
+  {
+    "revision": "b3fdc128b766477a0da0ce02b204c10d",
+    "url": "/employee-service/static/media/meeting.b3fdc128.png"
+  },
+  {
+    "revision": "2454b9d80443fe24e32e485f86a158e6",
+    "url": "/employee-service/static/media/search.2454b9d8.png"
+  },
+  {
+    "revision": "ded6ca5644a2ee31ecc8cdcbc526cf89",
+    "url": "/employee-service/static/media/request.ded6ca56.png"
+  },
+  {
+    "revision": "18cbd3721a650cef93f7d7b64a92ffa6",
+    "url": "/employee-service/static/media/graph.18cbd372.png"
+  },
+  {
+    "revision": "724dd001428c6eecabd2397d5fc8b773",
+    "url": "/employee-service/static/media/search-icon.724dd001.png"
+  },
+  {
+    "revision": "222820d2bdf8469d1b38d0505f7140ff",
+    "url": "/employee-service/index.html"
+  }
+];
